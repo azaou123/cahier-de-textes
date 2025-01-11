cba0ff3651d37ff305811d9d9aaa2b1ca692ccb7
azaou123 <azawbadr@gmail.com>
2025-01-11 01:35:29 +0100

    fix bug in edit page

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Filiere;
 use Illuminate\Database\Seeder;
+use Faker\Factory; // Import Faker Factory
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Create a Faker instance
+        $faker = Factory::create();
 
+        // Step 1: Seed users (students, professors, admins)
         $this->call([
             UsersTableSeeder::class,
+        ]);
+
+        // Step 2: Seed filieres
+        $this->call([
             FilieresTableSeeder::class,
+        ]);
+
+        // Step 3: Assign students to filieres
+        $filieres = Filiere::pluck('ID_Filiere')->toArray();
+        $students = User::where('Role', 'etudiant')->get();
+
+        foreach ($students as $student) {
+            $student->ID_Filiere = $faker->randomElement($filieres); // Use the Faker instance
+            $student->save();
+        }
+
+        // Step 4: Seed other tables
+        $this->call([
             CoursTableSeeder::class,
             SallesTableSeeder::class,
             SeancesTableSeeder::class,
